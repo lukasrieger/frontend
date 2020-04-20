5280ed9fcc8b304c7834e599a0824c47217d371d
Lukas Rieger <lukasrieger07@gmail.com>
2020-04-20 21:00:18 +0200

    Added option to build backend separately form the frontend.

--- src/main/kotlin/controller/ArticleController.kt
+++ src/main/kotlin/controller/ArticleController.kt
@@ -1,12 +1,15 @@
 package controller
 
 import ErrorEvent
+import arrow.core.Either
 import arrow.core.ValidatedNel
 import model.Article
 import mu.KotlinLogging
 import org.koin.core.KoinComponent
-import processErrorWith
-import repository.*
+import repository.ArticleRepository
+import repository.PrimaryKey
+import repository.ValidArticle
+import repository.mapAsyncV
 import tornadofx.Controller
 import validation.ArticleValidationError
 import validation.ArticleValidator
@@ -25,8 +28,10 @@ object ArticleController : KoinComponent, Controller() {
      * @param article Article
      * @return Completable
      */
-    suspend fun saveArticle(article: ValidArticle): Result<PrimaryKey<Article>> =
-        articleRepository.update(article).processErrorWith { ErrorEvent.CouldNotUpdateArticle(article.a, it) }
+    suspend fun saveArticle(article: ValidArticle): Either<ErrorEvent.CouldNotUpdateArticle, PrimaryKey<Article>> =
+        articleRepository.update(article).mapLeft {
+            ErrorEvent.CouldNotUpdateArticle(article.a, it)
+        }
 
     /**
      * Attempt to validate the given article first, if the validation succeeds save the article in the database,
@@ -34,18 +39,18 @@ object ArticleController : KoinComponent, Controller() {
      * @param article Article
      * @return ValidatedNel<ArticleValidationError, Result<PrimaryKey<Article>>>
      */
-    suspend fun validateAndSaveArticle(article: Article): ValidatedNel<ArticleValidationError, Result<PrimaryKey<Article>>> =
-        validator
-            .validate(article)
-            .mapAsyncV(::saveArticle)
+    suspend fun validateAndSaveArticle(
+        article: Article
+    ): ValidatedNel<ArticleValidationError, Either<ErrorEvent.CouldNotUpdateArticle, PrimaryKey<Article>>> =
+        validateMap(article, ::saveArticle)
 
 
     /**
      * Creates the given article in the database
      * @param article Article
      */
-    suspend fun createArticle(article: ValidArticle): Result<Article> =
-        articleRepository.create(article).processErrorWith { ErrorEvent.CouldNotCreateArticle(article.a, it) }
+    suspend fun createArticle(article: ValidArticle): Either<ErrorEvent.CouldNotCreateArticle, Article> =
+        articleRepository.create(article).mapLeft { ErrorEvent.CouldNotCreateArticle(article.a, it) }
 
     /**
      * Attempt to validate the given article first, if the validation succeeds create the article in the database,
@@ -53,10 +58,19 @@ object ArticleController : KoinComponent, Controller() {
      * @param article Article
      * @return ValidatedNel<ArticleValidationError, Result<Article>>
      */
-    suspend fun validateAndCreateArticle(article: Article): ValidatedNel<ArticleValidationError, Result<Article>> =
+    suspend fun validateAndCreateArticle(
+        article: Article
+    ): ValidatedNel<ArticleValidationError, Either<ErrorEvent.CouldNotCreateArticle, Article>> =
+        validateMap(article, ::createArticle)
+
+
+    private suspend fun <T, V> validateMap(
+        article: Article,
+        f: suspend (ValidArticle) -> Either<T, V>
+    ): ValidatedNel<ArticleValidationError, Either<T, V>> =
         validator
             .validate(article)
-            .mapAsyncV(::createArticle)
-
+            .mapAsyncV(f)
 
 }
+
